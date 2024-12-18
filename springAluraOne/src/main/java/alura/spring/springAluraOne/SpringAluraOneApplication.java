e356a23bd514bb89dd90790551327deba24f9766
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-12-17 22:21:26 -0300

    feat: project spring

---
+++ springAluraOne/src/main/java/alura/spring/springAluraOne/SpringAluraOneApplication.java
@@ -0,0 +1,13 @@
+package alura.spring.springAluraOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAluraOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAluraOneApplication.class, args);
+	}
+
+}
---
+++ springAluraOne/src/test/java/alura/spring/springAluraOne/SpringAluraOneApplicationTests.java
@@ -0,0 +1,13 @@
+package alura.spring.springAluraOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAluraOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
