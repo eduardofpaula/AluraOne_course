a938dbbf6c0c3ada1e186cbcb8c4785fd6a35a97
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2025-01-03 22:37:57 -0300

    feat: service to consult in API

---
+++ aluraChallenge/src/main/java/literAlura/aluraChallenge/service/ConsumoAPI.java
@@ -0,0 +1,27 @@
+package literAlura.aluraChallenge.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoAPI {
+    // consumo de api com HttpClient, httprquest, httpresponse
+    public String obterDados(String endereco){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return response.body();
+    }
+
+}
